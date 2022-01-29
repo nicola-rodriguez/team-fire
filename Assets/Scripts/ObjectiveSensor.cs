b173b4b424ca962c4240e3659bc40ac530ff3b8d
Nicola Rodriguez <nicola.rodriguez@gmail.com>
2022-01-29 18:13:33 -0500

    Swap color on collision

--- Assets/Scripts/ObjectiveSensor.cs
+++ Assets/Scripts/ObjectiveSensor.cs
@@ -7,12 +7,18 @@ public class ObjectiveSensor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    //Upon collision with another GameObject, this GameObject will reverse direction
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        this.GetComponent<Collider2D>().enabled = false;
+        this.GetComponent<SpriteRenderer>().color = new Color(13,2,33,255);
     }
 }
