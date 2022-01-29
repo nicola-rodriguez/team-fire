ae9062cae87646d48a83d4526a710a208ac5452d
gabrielw <gabrielwilliams.me@gmail.com>
2022-01-29 17:58:26 -0500

    fixing some values for scale and getting ready to match to player set sizing

--- Assets/Scripts/SetPlayZone.cs
+++ Assets/Scripts/SetPlayZone.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class SetPlayZone : MonoBehaviour
 {
     public Camera gameCamera;
+    public float chosenHeight;
     public Transform levelBounds;
     // Start is called before the first frame update
     void Start()
     {
         //get the main camera
         gameCamera = Camera.main;
+        //set height on camera
+        gameCamera.orthographicSize = chosenHeight*.5f;
         //scale the level to match the screen
-        levelBounds.localScale = new Vector3(((10f * gameCamera.pixelWidth)/gameCamera.pixelHeight), 10, 1);
+        levelBounds.localScale = new Vector3(((chosenHeight * gameCamera.pixelWidth)/gameCamera.pixelHeight),chosenHeight, 1);
     }
 }
