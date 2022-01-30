8e6c245dbbc584bf8ca4577ea9e514843dc582f2
Nicola Rodriguez <nicola.rodriguez@gmail.com>
2022-01-30 11:47:16 -0500

    Adjusted prefab for painting

--- Assets/Scripts/Painting.cs
+++ Assets/Scripts/Painting.cs
@@ -8,9 +8,11 @@ public class Painting : MonoBehaviour
     public bool isPainting;   
     public GameObject brush;
     public GameObject prefabToPaint;
+    private LineRenderer lineRend;
     void Awake()
     {
         aList = new List<GameObject>();
+        lineRend = gameObject.GetComponent<LineRenderer>();
     }
 
     // Start is called before the first frame update
@@ -30,10 +32,16 @@ public class Painting : MonoBehaviour
         if (isPainting)
         {
             var aGameObject = Instantiate(prefabToPaint, brush.transform.position, Quaternion.identity);
-            /* LineRend.gameObject.SetActive(true);
-            LineRend.SetVertexCount(2);
-            LineRend.SetPosition(0, aList[aList.Count].transform.position);
-            LineRend.SetPosition(1, aGameObject.transform.position); */
+            /*
+            lineRend.gameObject.SetActive(true);
+            lineRend.positionCount = 2;
+            int last = aList.Count;
+            if (last > 0)
+            {
+                lineRend.SetPosition(0, aList[last-1].transform.position);
+                lineRend.SetPosition(1, aGameObject.transform.position);
+            }
+            */
             aList.Add(aGameObject);
         }
     }
